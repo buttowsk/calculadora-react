2d91f13e583ba4595533756853f32d88b7ce19fa
Vinícius Nunes <99284437+buttowsk@users.noreply.github.com>
2023-03-04 01:12:20 -0300

    Update styles.js

--- src/styles.js
+++ src/styles.js
@@ -17,27 +17,11 @@ export const Content = styled.div`
     border-radius: 10px;
     background: linear-gradient(90deg, #e3ffe7 0%, #d9e7ff 100%);
 
-
-    @media screen and (max-width: 375px) {
-        & {
-            padding: 20px 15px;
-            height: 580px;
-            border: 0;
-        }
-    }
-
 `
 
 export const Linha = styled.div`
     display: flex;
     justify-content: space-between;
     margin: 5% 0;
-
-    @media screen and (max-width: 320px) {
-        & {
-            margin 10% 0;
-        }
-    }
-
    
 `
